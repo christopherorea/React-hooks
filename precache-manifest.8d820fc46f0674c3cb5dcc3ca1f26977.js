0b918870a654e6e00b4507c388d6463ab2a24ad4
christopherorea <c28_09_98@hotmail.com>
2020-08-07 07:39:17 -0500

    Updates

---
+++ precache-manifest.8d820fc46f0674c3cb5dcc3ca1f26977.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cac52315bd23aa03e44b152b156b0d8",
+    "url": "/React-hooks/index.html"
+  },
+  {
+    "revision": "4d4c21a49e998f482e40",
+    "url": "/React-hooks/static/js/2.e1cd0750.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-hooks/static/js/2.e1cd0750.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1788b84bf4200210458b",
+    "url": "/React-hooks/static/js/main.2af78427.chunk.js"
+  },
+  {
+    "revision": "2dadadcd46e3c05243c4",
+    "url": "/React-hooks/static/js/runtime-main.3a78b73a.js"
+  }
+]);
